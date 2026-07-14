44bcfa61325194f354c692caf4b573659c68905a
Iviv122 <ivan.skrinnik.nomia@gmail.com>
2026-07-14 14:08:20 +0200

    level exit

--- objects/env/exit.gd
+++ objects/env/exit.gd
@@ -4,8 +4,8 @@ class_name Exit
 @export var next_ind : int = -1
 
 func _load() -> void:
-	if next_ind < 0:
-		SceneManager.load_level(get_tree(),next_ind)
+	if next_ind >= 0:
+		SceneManager.load_level(get_tree(),next_ind-1)
 
 func try_load(other : Node2D) -> void:
 	if other is PlayerBody:
--- objects/globals/my_scene_manager.gd
+++ objects/globals/my_scene_manager.gd
@@ -9,14 +9,14 @@ class_name MySceneManager
 func load_playground(tree : SceneTree) -> void:
 	if !tree:
 		return
-	tree.change_scene_to_packed(playground)
+	tree.change_scene_to_packed.call_deferred(playground)
 
 func load_main_menu(tree : SceneTree) -> void:
 	if !tree:
 		return
-	tree.change_scene_to_packed(main_menu)
+	tree.change_scene_to_packed.call_deferred(main_menu)
 
 func load_level(tree : SceneTree,index : int) -> void:
 	if !tree:
 		return
-	tree.change_scene_to_packed(levels[index])
+	tree.change_scene_to_packed.call_deferred(levels[index])
