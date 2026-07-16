7f1252d63ad39c2f0c2dfa41ae4314254d02cdc3
Iviv122 <ivan.skrinnik.nomia@gmail.com>
2026-07-16 09:51:50 +0200

    moving object proper reset

--- objects/env/moving_object.gd
+++ objects/env/moving_object.gd
@@ -4,6 +4,7 @@ class_name MovingRoot
 @export var points : Array[Vector2]
 @export var speed : float = 0
 @export var loop : bool = true
+@export var reset_bus : ItemBus
 
 var t : Tween
 var ind : int
@@ -15,8 +16,13 @@ func _ready() -> void:
 	init_pos = global_position
 	move_iter()
 	reached.connect(move_iter)
+	reset_bus.level_reset.connect(reset)
 
 func reset() -> void:
+	if t:
+		t.stop()
+		t.kill()
+	print("reset")
 	global_position = init_pos
 	ind = 0
 
